03d23ffa9e721919190e315c6d95c21568734fa9
Felipe Marques <felipelamarques@outlook.com>
2024-09-04 14:58:27 -0300

    general update C# folder

---
+++ C#/Pim-testarea_solution/Pim-testarea/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Pim_testarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_sqlconnection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_sqlconnection
+            // 
+            this.button_sqlconnection.Location = new System.Drawing.Point(12, 12);
+            this.button_sqlconnection.Name = "button_sqlconnection";
+            this.button_sqlconnection.Size = new System.Drawing.Size(153, 23);
+            this.button_sqlconnection.TabIndex = 0;
+            this.button_sqlconnection.Text = "Conectar SQL";
+            this.button_sqlconnection.UseVisualStyleBackColor = true;
+            this.button_sqlconnection.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_sqlconnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_sqlconnection;
+    }
+}
+
---
+++ C#/Pim-testarea_solution/Pim-testarea/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pim_testarea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Declaração das variáveis
+            //string server = "PC-B450M";  // Pode ser um endereço IP ou o nome do servidor
+            //string serverdoor = "8391";
+            //string database = "ampereti_db";
+            //string username = "sa";
+            //string password = "Admin1234";
+
+            // Defina a string de conexão com os detalhes do seu servidor SQL e banco de dados.
+            string connectionString = "Server=PC-B450M;Database=ampereti_db;User Id=sa;Password=Admin1234;";
+
+            // Cria uma conexão com o banco de dados.
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Abre a conexão com o banco de dados.
+                    connection.Open();
+                    Console.WriteLine("Conexão com o banco de dados estabelecida com sucesso!");
+
+                    // Aqui você pode executar consultas, comandos, etc.
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ocorreu um erro ao conectar ao banco de dados: " + ex.Message);
+                }
+            }
+        }// A conexão é fechada automaticamente ao sair do bloco 'using'.
+    }
+}
+
